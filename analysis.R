828d81c25d8294e3909f368cb8a5dc85b6a65a74
Louis Teitelbaum <louis@teitelbaum.us>
2023-04-24 17:16:24 +0300

    fancy filtering

--- analysis.R
+++ analysis.R
@@ -1,21 +1,27 @@
 library(tidyverse)
 library(lubridate)
 
-covid <- bind_rows(read_csv("test.csv") %>% rename(Id = ForecastId) %>% 
-                     mutate(ConfirmedCases = NA, 
-                            Fatalities = NA), 
-                   read_csv("train.csv"))
+covid <- read_csv("train.csv")
 
 # Select the columns we need
 covid <- covid %>% 
-  arrange(Date) %>% 
-  mutate(country = `Country/Region`, date = Date, cases = ConfirmedCases) %>% 
+  arrange(`Country/Region`, Date) %>% 
+  rename(country = `Country/Region`, date = Date, cases = ConfirmedCases) %>% 
+  group_by(country, date) %>% 
+  summarise(cases = sum(cases)) %>% 
   select(country, date, cases)
 
 # Filter only countries with at least 50 cases and at least 14 days of data afterwards
-covid %>% 
+covid <- covid %>% 
   group_by(country) %>% 
-  filter(max(cases, na.rm = TRUE) > 50)
+  filter(cummax(cases) > 50) %>% 
+  arrange(country, date) %>% 
+  mutate(day = 1:n()) %>%
+  filter(day %in% c(1, 14)) %>% 
+  pivot_wider(id_cols = 'country', names_from = 'day', values_from = 'cases', names_prefix = "cases_day") %>% 
+  mutate(growth = cases_day14 - cases_day1) %>% 
+  drop_na()
+
 
 bigfive <- read_tsv("/Users/louisteitelbaum/Projects/covid_bigfive/data-final.csv")
 bigfive <- bigfive %>% select(EXT1:OPN10, country)
